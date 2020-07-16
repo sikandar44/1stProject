31ace90a20fbc8d2291818d16a24f4c4d0687ae4
sikandar44 <meersikanderali44@gmail.com>
2020-07-16 06:02:01 +0500

    First commit

---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+
+export default class TourList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello From Tour List </h1>
+                <Tour/>   
+            </div>
+        )
+    }
+}
